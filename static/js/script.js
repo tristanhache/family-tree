c524b14a0f8f30a15f1f4b49c0e49c3bc103c3d3
tristanhache <120027436+tristanhache@users.noreply.github.com>
2023-03-25 22:36:34 +0000

    trust

--- static/js/script.js
+++ static/js/script.js
@@ -3,7 +3,7 @@ $('form').submit( function ()
     if( $('input[name=login]').val() == 'username' && $('input[name=password]').val() == 'password' )
     {
         alert('You are logged in!');
-        window.location.href="sample.html";
+        window.location.href="page-2.html";
     }
               
     else
